97f57564101ceed6a29fae087d93a7ff2ed84257
zhch <2078592182@qq.com>
2019-07-08 08:58:58 +0800

    personal floder

---
+++ personTestFolder/huiwenChar/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace huiwenChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //huiWenTest();
+
+            int[] scores = new int[] { 97, 92, 81, 87 };
+
+            IEnumerable<int> scorequery =
+                from score in scores
+                where score > 90
+                select score;
+
+            foreach (var item in scorequery)
+            {
+                Console.WriteLine(item + "   ");
+                Console.ReadKey();
+            }  
+
+        }
+        public static void  huiWenTest()
+        {
+            string str = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            Queue<char> queue = new Queue<char>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                stack.Push(str[i]);
+                queue.Enqueue(str[i]);
+            }
+
+            bool ishuiwen = true;
+            while(stack.Count > 0)
+            {
+                if (stack.Pop() != queue.Dequeue())
+                {
+                    ishuiwen = false;
+                    break;
+                }
+            }
+
+            Console.WriteLine("输入的字符串是回文嘛？ --"  + ishuiwen);
+            Console.ReadKey();
+
+        }
+    }
+}
